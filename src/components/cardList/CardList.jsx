60eba7625f3157f52ced8afa9e41d3e19b5fd998
kazuki2000 <pkazuki2000@icloud.com>
2024-04-13 20:45:41 +0900

    ファースト

---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+import Image from "next/image";
+
+const CardList = async () => {
+  return <div className={styles.container}>CardList</div>;
+};
+
+export default CardList;
---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const CategoryList = async () => {
+  return <div className={styles.container}>CategoryList</div>;
+};
+
+export default CategoryList;
